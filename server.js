ae0f290b911c727e72268b1fbc1736580263dd48
RishikaReddy123 <chrishikareddy26@gmail.com>
2025-03-05 09:50:13 +0530

    Modified

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const mongoose = require("mongoose");
 const PORT = process.env.PORT || 8000;
+
+app.use(cors());
 
 require("dotenv").config();
 
